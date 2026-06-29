a58ff4a26772edeff128c73d557e5a80b90b7eaa
turbillon50 <turbillon50@gmail.com>
2026-06-29 17:31:59 -0500

    remove: obsoleto

--- src/components/layouts/app-layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-import { DemoModeSwitcher } from '@/components/demo-mode-switcher';
-
-export default function AppLayout({ children }: { children: ReactNode }) {
-  return <DemoModeSwitcher>{children}</DemoModeSwitcher>;
-}
