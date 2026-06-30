2b1bd3bb3a2ab7199eecbf1a4d2027e52af9f0bd
turbillon50 <turbillon50@gmail.com>
2026-06-29 22:00:57 -0500

    feat: registro service worker

--- src/components/sw-register.tsx
+++ src/components/sw-register.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect } from 'react';
 
-export default function SWRegister() {
+export default function SwRegister() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js').catch(() => {});
