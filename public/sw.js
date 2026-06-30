db3b724e216b8df9e0c13cc9773dea300b349dea
turbillon50 <turbillon50@gmail.com>
2026-06-29 22:00:59 -0500

    feat: service worker cache-first

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,9 @@
 const CACHE = 'kyo-v1';
-const PRECACHE = ['/', '/manifest.json'];
-
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then((c) =>
-      Promise.allSettled(PRECACHE.map((u) => c.add(u)))
-    )
-  );
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['/'])));
 });
-
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-
 self.addEventListener('fetch', (e) => {
-  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => r))
   );
 });
